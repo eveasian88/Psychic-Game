8b79b56f5d38c6a66c8a8fc0f6dd4fe3534e645d
Susye Weng-Reeder <weng-reeder@hotmail.com>
2018-10-03 20:52:16 -0700

    alert wins

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,50 +11,63 @@ var computerGuess = [];
 
 
 // initial computer letter
-window.onload = function() {
+window.onload = function () {
 	var compGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 	computerGuess.push(compGuess);
-	console.log(computerGuess[0]);
+	console.log(computerGuess[0]); // used for debugging
 }
 
 
 // game
-document.onkeyup = function(event) {
+document.onkeyup = function (event) {
 	var playerGuess = event.key;
 	lettersGuessed.push(playerGuess);
-	console.log(computerGuess[0]);
+	console.log(computerGuess[0]); //used for debugging
 
-if ((playerGuess === computerGuess[0]) && (guessesLeft > 0)) {
-	wins++;
-	guessesLeft = 9;
-	lettersGuessed.length = 0;
-	computerGuess.length = 0;
-	var compGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-	computerGuess.push(compGuess);
-	console.log(computerGuess[0]);
-}
+	if ((playerGuess === computerGuess[0]) && (guessesLeft > 0)) {
+		wins++;
+		alert(computerGuess[0]);
+		guessesLeft = 9;
+		lettersGuessed.length = 0;
+		computerGuess.length = 0;
+		var compGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+		computerGuess.push(compGuess);
+		console.log(computerGuess[0]);
+	}
 
-else if ((playerGuess !== computerGuess[0]) && (guessesLeft > 0)) {
-	guessesLeft = guessesLeft-1;
-}
-
-else {
-	losses++;
-	guessesLeft = 9;
-	lettersGuessed.length = 0;
-	computerGuess.length = 0;
-	var compGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-	computerGuess.push(compGuess);
-	console.log(computerGuess[0]);
-}
+	else if ((playerGuess !== computerGuess[0]) && (guessesLeft > 0)) {
+		guessesLeft = guessesLeft - 1;
+	}
 
 
-var html = "<p>Guess what letter I'm thinking of _ !</p>" +
-          "<p>Wins: " + wins + "</p>" +
-          "<p>Losses: " + losses + "</p>" +
-          "<p>Guesses left: " + guessesLeft + "</p>" +
-          "<p>Your guesses so far: " + lettersGuessed + "</p>";
- 
-document.querySelector("#game").innerHTML = html;
+	else {
+		losses++;
+		guessesLeft = 9;
+		lettersGuessed.length = 0;
+		computerGuess.length = 0;
+		var compGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+		computerGuess.push(compGuess);
+		console.log(computerGuess[0]);
+	}
+
+	document.onkeydown = function (event) {
+		var keysRemoved = event.keyCode || event.charCode;
+	
+		if (keysRemoved == 8 || keysRemoved == 46);
+		return false;
+	} // need to work on this part
+
+	// else {
+
+	// }
+
+
+	var html = "<p>Guess what letter I'm thinking of _ !</p>" +
+		"<p>Wins: " + wins + "</p>" +
+		"<p>Losses: " + losses + "</p>" +
+		"<p>Guesses left: " + guessesLeft + "</p>" +
+		"<p>Your guesses so far: " + lettersGuessed + "</p>";
+
+	document.querySelector("#game").innerHTML = html;
 
 }
