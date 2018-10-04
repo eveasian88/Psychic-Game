e5c055417f55bf29d1e1d0bd36af9a0d5fb1b9c1
Susye Weng-Reeder <weng-reeder@hotmail.com>
2018-10-03 23:37:19 -0700

    good keys only

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,67 +7,61 @@ var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
 var lettersGuessed = [];
-var computerGuess = [];
+var secretLetter = "a";
 
 
 // initial computer letter
 window.onload = function () {
-	var compGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-	computerGuess.push(compGuess);
-	console.log(computerGuess[0]); // used for debugging
+	secretLetter = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+	secretLetter.push(compGuess);
+	console.log(secretLetter); // used for debugging
 }
 
 
 // game
 document.onkeyup = function (event) {
 	var playerGuess = event.key;
-	lettersGuessed.push(playerGuess);
-	console.log(computerGuess[0]); //used for debugging
+	console.log(computerChoices);
+	if (computerChoices.includes(playerGuess)) {
+		console.log("good key");
+		
+		lettersGuessed.push(playerGuess);
 
-	if ((playerGuess === computerGuess[0]) && (guessesLeft > 0)) {
-		wins++;
-		alert(computerGuess[0]);
-		guessesLeft = 9;
-		lettersGuessed.length = 0;
-		computerGuess.length = 0;
-		var compGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-		computerGuess.push(compGuess);
-		console.log(computerGuess[0]);
+		if ((playerGuess === secretLetter) && (guessesLeft > 0)) {
+			wins++;
+			alert("Wow, you guessed right! You may be psychic.");
+			guessesLeft = 9;
+			lettersGuessed.length = 0;
+			secretLetter.length = 0;
+			var compGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+			secretLetter.push(compGuess);
+			console.log(secretLetter);
+		}
+		else if ((playerGuess !== secretLetter) && (guessesLeft > 0)) {
+			guessesLeft = guessesLeft - 1;
+		}
+		else {
+			losses++;
+			guessesLeft = 9;
+			lettersGuessed.length = 0;
+			secretLetter.length = 0;
+			var compGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+			secretLetter.push(compGuess);
+			console.log(secretLetter);
+		}
+		var html = "<p>Guess what letter I'm thinking of _ !</p>" +
+			"<p>Wins: " + wins + "</p>" +
+			"<p>Losses: " + losses + "</p>" +
+			"<p>Guesses left: " + guessesLeft + "</p>" +
+			"<p>Your guesses so far: " + lettersGuessed + "</p>";
+
+		document.querySelector("#game").innerHTML = html;
 	}
-
-	else if ((playerGuess !== computerGuess[0]) && (guessesLeft > 0)) {
-		guessesLeft = guessesLeft - 1;
-	}
-
 
 	else {
-		losses++;
-		guessesLeft = 9;
-		lettersGuessed.length = 0;
-		computerGuess.length = 0;
-		var compGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-		computerGuess.push(compGuess);
-		console.log(computerGuess[0]);
+		alert("press alphabet letters only")
 	}
+	console.log(secretLetter[0]); //used for debugging
 
-	document.onkeydown = function (event) {
-		var keysRemoved = event.keyCode || event.charCode;
 	
-		if (keysRemoved == 8 || keysRemoved == 46);
-		return false;
-	} // need to work on this part
-
-	// else {
-
-	// }
-
-
-	var html = "<p>Guess what letter I'm thinking of _ !</p>" +
-		"<p>Wins: " + wins + "</p>" +
-		"<p>Losses: " + losses + "</p>" +
-		"<p>Guesses left: " + guessesLeft + "</p>" +
-		"<p>Your guesses so far: " + lettersGuessed + "</p>";
-
-	document.querySelector("#game").innerHTML = html;
-
 }
